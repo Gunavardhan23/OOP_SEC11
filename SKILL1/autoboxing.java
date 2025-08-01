3dfde6a92f4583e51191979f26f1fabe693374a5
Gunavardhan23 <2420080023@klh.edu.in>
2025-08-01 11:02:30 +0530

    Add files via upload

--- SKILL1/autoboxing.java
+++ SKILL1/autoboxing.java
@@ -1,1 +1,13 @@
+package wrapperclass;
 
+public class autoboxing {
+
+	public static void main(String[] args) {  
+	    int a=20;  
+	   Integer i=Integer.valueOf(a);
+	   Integer j=a;  
+	   System.out.println(a+" "+i+" "+j);  
+
+	}
+
+}
---
+++ SKILL1/unboxing.java
@@ -0,0 +1,14 @@
+package wrapperclass;
+
+public class unboxing {
+
+	public static void main(String[] args) {
+	    Integer a=new Integer(3);    
+	    int i=a.intValue(); 
+	    int j=a;    
+	    System.out.println(a+" "+i+" "+j);    
+
+
+	}
+
+}
