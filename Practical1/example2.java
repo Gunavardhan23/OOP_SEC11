d74da01758ea41eb67efd2aa2877fb62a813526e
Gunavardhan23 <2420080023@klh.edu.in>
2025-08-01 11:01:12 +0530

    Add files via upload

---
+++ Practical1/example2.java
@@ -0,0 +1,10 @@
+package Primitivedatatypes;
+
+public class example2 {
+
+	public static void main(String[] args) {
+String greeting="Hello, World!";
+System.out.println(greeting);
+	}
+
+}
---
+++ Practical1/explicitcasting.java
@@ -0,0 +1,12 @@
+package Typecasting;
+
+public class explicitcasting {
+
+	public static void main(String[] args) {
+		double decimalNumber=9.78;
+		int wholeNumber;
+		wholeNumber=(int)decimalNumber;
+		System.out.println("Double value:"+decimalNumber);
+		System.out.println("Integer value after casting:"+wholeNumber);
+	}
+}
